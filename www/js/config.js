f50abfb3fe9c98ebe8cffaf37b61bc52937dd53b
Sherwin Jupiter <jupiter@pivotmobileapp.com>
2017-01-20 11:56:16 -0400

    eee

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={	
+	'ApiUrl':"http://appy-tite.com/driver/api",					
+	'DialogDefaultTitle':"Appy-tite Driver",	
+	'PushProjectID':"311412841455",	
+	'APIHasKey':"fed7b441b349bae8f146711fbd215e90"
+};
